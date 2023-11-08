0a00c1a2703a2565da8dd68dd79ae6f76c67c8f1
daurelie <aurelien.dugast@ynov.com>
2023-11-08 10:09:01 +0100

    myHangman

---
+++ jeu/txt.go
@@ -0,0 +1,37 @@
+package hangmanlesbg
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+)
+
+func SelectWord(s string) []string {
+	content, err := os.ReadFile(s)
+	if err != nil {
+		fmt.Println("file error")
+		return []string(nil)
+	}
+	list := ByteToString(content)
+	mot := list[rand.Intn(len(list)-1)]
+	run := []rune(mot)
+	str := []string{}
+	for i := 0; i < len(mot); i++ {
+		str = append(str, string(run[i]))
+	}
+	return str
+}
+
+func ByteToString(b []byte) []string {
+	word := ""
+	result := []string{}
+	for _, i := range b {
+		if string(i) == "\n" {
+			result = append(result, word)
+			word = ""
+		} else {
+			word += string(i)
+		}
+	}
+	return result
+}
